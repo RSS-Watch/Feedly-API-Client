02d7a787aa0ca42267ffe1df36a30deb6cfe5458
Dennis Effing <dennis.effing@hotmail.com>
2015-05-08 17:18:54 +0200

    Added List of EntryIDs for several entries and continuation for pagination.

--- src/main/java/org/feedlyapi/model/Stream.java
+++ src/main/java/org/feedlyapi/model/Stream.java
@@ -6,15 +6,28 @@ import java.util.List;
 public class Stream {
 
     private String id;
+    private List<String> ids;
     private String title;
+
+    private String continuation;
+
     private List<Article> items;
 
     public String getId() {
         return id;
     }
 
+    public List<String> getIds() {
+        return ids;
+    }
+
     public String getTitle() {
         return title;
+    }
+
+    public String getContinuation()
+    {
+        return continuation;
     }
 
     public List<Article> getItems() {
