489576ae8afefacccd2be9ecd4876bf765e00489
Dennis Effing <dennis.effing@hotmail.com>
2015-05-08 19:29:30 +0200

    Added constructor, setters and equals().

--- src/main/java/org/feedlyapi/model/Subscription.java
+++ src/main/java/org/feedlyapi/model/Subscription.java
@@ -5,6 +5,11 @@ import com.google.gson.annotations.SerializedName;
 @SuppressWarnings("unused")
 public class Subscription {
 
+    public Subscription(String feedId, String title) {
+        this.setFeedId(feedId);
+        this.setTitle(title);
+    }
+
     @SerializedName("id")
     private String feedId;
     private String title;
@@ -15,5 +20,24 @@ public class Subscription {
 
     public String getTitle() {
         return title;
+    }
+
+    public void setFeedId(String feedId) {
+        this.feedId = !feedId.startsWith("feed/") ? feedId : "feed/" + feedId;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Subscription that = (Subscription) o;
+
+        return feedId.equals(that.feedId);
+
     }
 }
